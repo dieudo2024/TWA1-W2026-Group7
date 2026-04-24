2ae938688b2251e46f72297d5e5b9ec5b30710c3
Dieudonné Bondo <dieudobond@gmail.com>
2026-04-23 22:08:37 -0400

    Add host role in User.

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema(
 		},
 		role: {
 			type: String,
-			enum: ['guest', 'user', 'admin'],
+			enum: ['guest', 'user', 'host', 'admin'],
 			default: 'guest',
 		},
 	},
