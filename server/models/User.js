0387da177d5fca6043b02f586007b335ce541c0e
Emmanuelle <EmmanuelleIsis.Mbeug@stu.crc-lennox.qc.ca>
2026-04-23 21:46:11 -0400

    changed the roles in User.js

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ const userSchema = new mongoose.Schema(
 		},
 		role: {
 			type: String,
-			enum: ['guest', 'host', 'admin'],
+			enum: ['guest', 'user', 'admin'],
 			default: 'guest',
 		},
 	},
